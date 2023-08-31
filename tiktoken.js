3f5d8431f703b31317ef98487e10b728577e0336
Manu <manu@mercenaries.ai>
2023-08-31 12:53:57 +0800

    Added tiktoken.js

---
+++ tiktoken.js
@@ -0,0 +1,23 @@
+//@ts-check
+import { encode } from 'gpt-tokenizer'
+
+// https://www.npmjs.com/package/gpt-tokenizer 
+// By default, importing from gpt-tokenizer uses cl100k_base encoding, used by gpt-3.5-turbo and gpt-4.
+
+export function countTokens(text)
+{
+
+    const tokens = encode(text); //encoding.encode(text);
+    if (tokens !== null && tokens !== undefined && tokens.length > 0)
+    {
+        const num_tokens = tokens.length;
+        return num_tokens;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+//export { countTokens }
